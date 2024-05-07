3ec1617c92536258f008233040aadddfad7fff2f
Abishek <88036229+bvabishek@users.noreply.github.com>
2024-05-07 15:45:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const employeeController = require("./src/controller/employeeController");
 const cors = require('cors')
 
 app.use(express.json());
-app.use(cors({ origin: '*' }));
+app.use(cors({
+  origin: '*',
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization'
+}));
+
 
 mongoose
   .connect("mongodb+srv://bvabishek:S32Sc2csjaJpcRcU@cluster0.cfnialp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
