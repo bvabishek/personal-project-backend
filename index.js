983253228668a9b1df2475870840e5471b4c21e9
Abishek <88036229+bvabishek@users.noreply.github.com>
2024-05-07 12:53:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const employeeController = require("./src/controller/employeeController");
-const cors = require("cors")
+const cors = require('cors')
 
 app.use(express.json());
-  app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
-    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-    });
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 mongoose
   .connect("mongodb+srv://bvabishek:S32Sc2csjaJpcRcU@cluster0.cfnialp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => {
@@ -21,12 +19,6 @@ mongoose
   .catch(() => {
     console.log("connection failed");
   });
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 
 app.get("/api/employeeinfo", employeeController.getAllEmployees);
