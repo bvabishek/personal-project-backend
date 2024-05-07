5c3f09fb34c6513c42682ed1e486c6bba7cdd1d9
Abishek <88036229+bvabishek@users.noreply.github.com>
2024-05-07 11:30:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const employeeController = require("./src/controller/employeeController");
 const cors = require("cors")
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
 
 mongoose
   .connect("mongodb+srv://bvabishek:S32Sc2csjaJpcRcU@cluster0.cfnialp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
