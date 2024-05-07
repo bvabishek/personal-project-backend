b2bf7bd35290709155b396a3a1486a8f358b9df2
Abishek <88036229+bvabishek@users.noreply.github.com>
2024-05-07 10:37:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ mongoose
     console.log("connection failed");
   });
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 app.get("/api/employeeinfo", employeeController.getAllEmployees);
 app.post("/api/employeeinfo", employeeController.createEmployee);
